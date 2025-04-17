2402fb5926aefe550bd3388d49b9813d3106c6bf
HopeSaurus <hopeXime@outlook.com>
2025-04-17 15:25:58 -0600

    Add jwt generation and validation with test units

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestMakeJWT(t *testing.T) {
+	expectedID := "83064af3-bb81-4514-a6d4-afba340825cd"
+	uuid, err := uuid.Parse(expectedID)
+	if err != nil {
+		t.Errorf("Invalid uuid")
+	}
+
+	token, err := MakeJWT(uuid, "test", time.Hour)
+	if err != nil {
+		t.Errorf("Error creating the jwt: %s", err)
+	}
+	resultID, err := ValidateJWT(token, "test")
+	if err != nil {
+		t.Errorf("Invalid jwt %s", err)
+	}
+	if resultID != uuid {
+		t.Errorf(`Error %s is different than %s`, resultID, uuid)
+	}
+}
---
+++ internal/auth/jwt.go
@@ -0,0 +1,41 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String()})
+
+	ss, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return ss, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if !token.Valid {
+		return uuid.Nil, fmt.Errorf("invalid token")
+	}
+
+	uuidS, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return uuid.Parse(uuidS)
+}
